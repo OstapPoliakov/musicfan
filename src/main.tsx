40d50a85410442573b0799607e05e1d5ae35217b
Ostap Poliakov <elcortezos@gmail.com>
2025-09-30 23:05:58 +0300

    Configure RTK Store, configure basic RTK Query hook

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-//import { StrictMode } from "react";
+import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./app/ui/App/App";
 import { BrowserRouter } from "react-router";
+import { store } from "./app/model/store";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
